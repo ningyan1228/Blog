2ff7a3f0fdac85dce2d06ffd8a2ce563c0390df9
ningyan1228 <wyzxn21@163.com>
2025-04-05 00:10:22 +0800

    test1

--- 类和对象-封装/类和对象(封装）/类和对象(封装）/C++对象模型和this指针.cpp
+++ 类和对象-封装/类和对象(封装）/类和对象(封装）/C++对象模型和this指针.cpp
@@ -15,26 +15,42 @@ using namespace std;
 */
 //成员变量和成员函数分开存储
 class Person {
-    
+public:   
+    int m_A; //非静态成员变量  属于类的对象上  只有他属于
 
+    static int m_B; //静态成员变量，不属于类的对象上
 
+    void func(){} //非静态成员函数 不属于类对象上
+    /*不属于类对象上意思是没有增加空间输出这段语句的时候：
+     cout << "size of p= " << sizeof(p) << endl;
+    */
+    static void func2() {  //不属于类的对象上
 
+    }
 };
 
-void test01() {
-    Person p;
+int Person::m_B = 10;
 
+void test01() {
+    Person p;  //class 里面属性啥也不写的情况下
+    //虽然是空对象，但是也不能占用同样的位置
     //空对象占用内存空间为  1 字节；
     //因为c++编译器会给每个空对象也分配一个字节空间，
-    // 每个空对象都需要占用独一无二的内存空间
+    // 每个空对象也应该有一个独一无二的内存空间
     // 是为了区分空对象占内存的位置
     cout << "size of p= " << sizeof(p) << endl;
 }
 
-int main() {
+void test02() {
+    Person p;
+    cout << "size of p= " << sizeof(p) << endl;
 
-    test01();
+}
 
+int main1() {
+
+    //test01();
+    test02();
     system("pause");
     return 0;
 }
